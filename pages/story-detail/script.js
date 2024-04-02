18c46c3368b44277f2b8b1a59626ca70f53910d2
khonsreyvorleak5796-4 <sreyvorleak.khon@institute.pse.ngo>
2024-04-02 10:25:13 +0700

    new

---
+++ pages/story-detail/script.js
@@ -0,0 +1,21 @@
+let titleDiv=document.getElementById( "title");
+titleDiv.innerHTML =("<h1>The Shoe That Flew</h1>");
+let paragraphDiv=document.getElementById("paragraph");
+paragraphDiv.innerHTML =("<p>When Ollie does a GIANT kick one sunny day, it's not just the ball that goes flying!</p>");
+// let img=document.createElementById("img");
+// img.scr = "Cinderella.jpeg" ;
+// img.alt = "Cinderella.jpeg" ;
+// img.width = 500 ;
+// img.height = 500 ;
+// imageContainer.appendchild(img)
+var element = document.getElementById ('title');
+element.style.color= 'blue';
+element.style.textAlign= 'center' ; 
+element.style.marginTop= '4%' ; 
+var paragraph = document.getElementById ('paragraph');
+paragraph.style.color = 'orange';
+paragraph.style.textAlign = 'center';
+paragraph.style.marginTop = '2%';
+
+
+
